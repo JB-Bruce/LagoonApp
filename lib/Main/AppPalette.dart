80421c65aa0fc953df81a3dcf16aac1e0f6d2ef3
JB-Bruce <B.JEAN-BAPTISTE@lahorde.school>
2025-06-10 18:21:00 +0200

    added color pallette

---
+++ lib/Main/AppPalette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppPalette
+{
+  static final Color backgroundColor = Colors.grey[900]!;
+  static final Color gradiant1 = Colors.blue[500]!;
+  static final Color gradiant2 = Colors.blue[300]!;
+  static final Color borderColor = Colors.grey[500]!;
+  static final Color transparentColor = Colors.transparent;
+}
